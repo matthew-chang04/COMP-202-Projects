919e0cdbfa607bb5c050a86fba0676c2ceb6a7ee
matthew-chang04 <matthewchang04@gmail.com>
2024-11-05 20:36:58 -0500

    Update assingment_3.py

--- assingment_3.py
+++ assingment_3.py
@@ -34,23 +34,54 @@ def string2list(string):
 
     for index in range(len(list)):
 
-        list[index] = temp_list[index * sublist_len: index * sublist_len + sublist_len]
+        list[index] = temp_list[index * sublist_len: (index * sublist_len) + sublist_len]
 
     return list
 
 def add_space(input_text):
-
+    
+    last_edit = 0
+    string_copy = input_text
     output_text = ""
-    for index in range(1,len(input_text)-2):
+    for index in range(1,len(input_text) -1):
 
-        if input_text[index - 1].lower() == input_text[index - 1] and input_text[index + 1].lower() == input_text[index + 1] and input_text[index] != input_text[index].lower():
-            replacement = input_text[:index]
+        if input_text[index] != input_text[index].lower() and input_text[index - 1].lower() == input_text[index - 1] and input_text[index + 1].lower() == input_text[index + 1] and input_text[index + 1] != " " and input_text[index - 1] != " ":
+            output_text = output_text + string_copy[last_edit:index] + " "
+            last_edit = index
+        elif index == len(input_text) - 2:
+            output_text += string_copy[last_edit:]
 
-            output_text += (replacement + " ")
 
     return output_text
+    
+def list2string(list):
 
-print(add_space("HelloMyNameIs"))
+    string_result = ""
 
+    for sublist in list:
+        for char in sublist:
+            string_result += char
+    
+    return add_space(string_result)
 
+def horizontal_flip(list):
 
+    new_list = []
+    for sublist in list:
+        new_list.append(sublist[::-1])
+
+    return new_list
+
+def transpose(list):
+
+    new_list = []
+    
+    for row in range(0, len(list)):
+        new_sublist = []
+        for column in range(0, len(list)):
+            new_sublist.append(list[column][row])
+        new_list.append(new_sublist)
+
+    return new_list
+
+def flip_list(list):
